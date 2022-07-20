1522da82321ef67523b917b04a3a2cae035ee896
Avecoder <92373885+Avecoder@users.noreply.github.com>
2022-07-20 05:52:09 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 export const githubUrl = 'https://github.com/Avecoder'
 export const telegramUrl = 'https://t.me/turbo_asm_bot'
 export const youtubeUrl = 'https://www.youtube.com/c/GrinSoft'
-export const baseURL = 'http://localhost:4444'
+export const baseURL = 'https://tasm-docs.herokuapp.com/'
