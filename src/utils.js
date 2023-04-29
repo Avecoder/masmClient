4d25bc4ecd7015eb19fc66947d2ca928a10e75b4
Avecoder <92373885+Avecoder@users.noreply.github.com>
2023-04-30 02:40:49 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 export const githubUrl = 'https://github.com/Avecoder'
 export const telegramUrl = 'https://t.me/turbo_asm_bot'
 export const youtubeUrl = 'https://www.youtube.com/c/GrinSoft'
-export const baseURL = 'https://tasmserver-production.up.railway.app/'
+export const baseURL = 'http://45.12.238.184:4444/'
