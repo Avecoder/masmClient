b787461a4afc16afbb686afc61642cac3bec5ce1
Avecoder <92373885+Avecoder@users.noreply.github.com>
2024-08-22 01:26:50 +0000

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 export const githubUrl = 'https://github.com/Avecoder'
 export const telegramUrl = 'https://t.me/turbo_asm_bot'
 export const youtubeUrl = 'https://www.youtube.com/c/GrinSoft'
-export const baseURL = 'https://api.hipsterjo.fun:4444/'
+export const baseURL = 'https://api.hipsterjo.fun/tasm/'
